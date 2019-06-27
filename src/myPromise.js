2f4071fcbf410cb90820a4786c70e53e0e936cfe
Siddharth <sidhbaha@gmail.com>
2019-06-27 23:51:37 +0530

    added implementation for resolve,reject,race and all

--- src/myPromise.js
+++ src/myPromise.js
@@ -3,12 +3,28 @@ class MyPromise{
     constructor(executor){
 
         let resolve = (value) => {
+
+            if(this.state !== 'pending')
+            {
+                return;
+            }
+
             this.state = 'resolved';
             this.value = value;
+            let then = value.then !== null ? v.then : null;
+            if(then instanceof Function){
+                then(resolve,reject);
+            }
+
             notifySubscribers();
         };
 
         let reject = (value) =>{
+            if(this.state !=='pending')
+            {
+                return;
+            }
+
             this.state = 'rejected';
             this.value = value;
             notifySubscribers();
@@ -60,6 +76,39 @@ class MyPromise{
             }
         });
 
+    }
+
+    static all(listPromises){
+        // wait for all of them to complete
+        let results = [];
+        let totalResolved = 0;
+
+       return MyPromise((resolve,reject) =>{
+          listPromises.forEach((promise)=>{
+              promise.then((result)=>{
+                  results.add(result);totalResolved++;
+                  if(totalResolved===listPromises.length) resolve(results);
+              }, (error) => reject(error));
+          })
+        });
+    }
+
+    static race(listPromise){
+
+        return MyPromise((resolve,reject)=>{
+            listPromise.forEach( promise =>{
+                promise.then(result => resolve(result),error => reject(error));
+            })
+        })
+
+    }
+
+    static resolve(value){
+        return MyPromise(resolve => resolve(value));
+    }
+
+    static reject(value){
+        return MyPromise((resolve,reject) => reject(value));
     }
 
 }
